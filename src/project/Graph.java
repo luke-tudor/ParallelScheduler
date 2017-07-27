65a2634ee0d3fa964812a4034486732f78f57bf4
Luke Tudor <ltud719@aucklanduni.ac.nz>
2017-07-27 12:21:39 +1200

    simplified Graph implementation by removing enum

--- src/project/Graph.java
+++ src/project/Graph.java
@@ -5,38 +5,24 @@ import java.util.List;
 
 public class Graph {
 	
-	private enum ElementType {NODE, EDGE}
-	
 	private List<Node> nodes = new ArrayList<Node>();
 	
 	private List<Edge> edges = new ArrayList<Edge>();
 	
-	private List<ElementType> order = new ArrayList<ElementType>();
+	private List<Object> order = new ArrayList<Object>();
 	
 	public void addNode(Node n) {
 		nodes.add(n);
-		order.add(ElementType.NODE);
+		order.add(n);
 	}
 	
 	public void addEdge(Edge e) {
 		edges.add(e);
-		order.add(ElementType.EDGE);
+		order.add(e);
 	}
 	
 	public Object[] getAllElements() {
-		Object[] elements = new Object[order.size()];
-		int nodeCount = 0;
-		int edgeCount = 0;
-		for (int i = 0; i < elements.length; i++) {
-			if (order.get(i) == ElementType.NODE) {
-				elements[i] = nodes.get(nodeCount);
-				nodeCount++;
-			} else {
-				elements[i] = edges.get(edgeCount);
-				edgeCount++;
-			}
-		}
-		return elements;
+		return order.toArray();
 	}
 
 }
