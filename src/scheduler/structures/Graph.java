de1821ba951e7c4fc91afd4f5513bc5f6ed4c81d
Max van duin <mvan439@aucklanduni.ac.nz>
2017-08-05 15:43:18 +1200

    updated Documentation

--- src/scheduler/structures/Graph.java
+++ src/scheduler/structures/Graph.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * This class represents a graph ADT.
+ * Provides a representation of a graph ADT.
  * 
  * @author Luke Tudor
  */
@@ -28,11 +28,23 @@ public class Graph {
 		graphName = name;
 	}
 	
+	/**
+	*
+	* Adds a node to the graph.
+	*
+	* @param n the Node object to add to the graph.
+	*/
 	public void addNode(Node n) {
 		nodes.put(n.getName(), n);
 		order.add(n);
 	}
 	
+	/**
+	*
+	* Adds an edge object to the graph
+	*
+	* @param e the edge object to add to the graph
+	*/
 	public void addEdge(Edge e) {
 		Node parent = nodes.get(e.getParent());
 		Node child = nodes.get(e.getChild());
@@ -42,6 +54,11 @@ public class Graph {
 		order.add(e);
 	}
 	
+	/**
+	* retrieves all graph elements (nodes and edges) in the order that they were added to the graph
+	*
+	* @return Object[] containing all nodes and edges of the graph
+	*/
 	public Object[] getAllElements() {
 		return order.toArray();
 	}
