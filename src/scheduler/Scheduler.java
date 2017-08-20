38d3847fcb993f4660f2775b990a158b84e53fbe
Luke Tudor <ltud719@aucklanduni.ac.nz>
2017-08-20 21:32:49 +1200

    removed some unused imports in Scheduler

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -1,8 +1,5 @@
 package scheduler;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
