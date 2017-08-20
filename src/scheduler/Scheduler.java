07d7fa9ae792d88adeb1d6ae3b8a53fbbc85cb05
Luke Tudor <ltud719@aucklanduni.ac.nz>
2017-08-20 21:11:50 +1200

    made A* work on multiple threads, not sure what impact this has on the CPU however

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import scheduler.io.InputParser;
 import scheduler.io.OutputFormatter;
@@ -24,6 +25,8 @@ public class Scheduler {
 	private Graph graph;
 	private int numProcessors;
 
+	private int numThreads;
+
 	// Queue that all threads are working off, it blocks if multiple threads wish to access it at once
 	private PriorityBlockingQueue<TreeNode> q = new PriorityBlockingQueue<>();
 	// Executor executes "processNode()" task
@@ -34,47 +37,63 @@ public class Scheduler {
 		this.graph = graph;
 		this.numProcessors = numProcessors;
 		exe = Executors.newFixedThreadPool(numThreads);
+		this.numThreads = numThreads;
 	}
 
 	/**
 	 * Computes the optimum processing schedule for the graph.
 	 */
 	public Graph computeSchedule() {
-		PriorityQueue<TreeNode> q = new PriorityQueue<>();
 		q.add(new TreeNode());
-		while (!q.isEmpty()) {
-			// Pop from priority queue
-			TreeNode current = q.remove();
-			// If current equals goal or complete solution, we have the optimal solution
-			// Uses height to determine whether a schedule is complete
-			if (current.getHeight() == graph.getAllNodes().size()) {
-				TreeNode tail = current;
-				while (tail.getNode() != null) {
-					tail.getNode().setProcessor(tail.getProcessor() + 1);
-					tail.getNode().setStart(tail.getStartTime());
-					tail = tail.getParent();
-				}
-				return graph;
-			}
+		for (int i = 0; i < numThreads; i++) {
+			exe.submit(new Runnable() {
 
-			// Find neighbouring nodes
-			Set<Node> neighbours = graph.getNeighbours(current);
-			for (Node n : neighbours) {
-				for (int i = 0; i < numProcessors; i++) {
-					TreeNode candidate = new TreeNode(current, n, i);
-					q.add(candidate);
+				@Override
+				public void run() {
+					while (true) {
+						while (!q.isEmpty()) {
+							// Pop from priority queue
+							TreeNode current = q.remove();
+							// If current equals goal or complete solution, we have the optimal solution
+							// Uses height to determine whether a schedule is complete
+							if (current.getHeight() == graph.getAllNodes().size()) {
+								TreeNode tail = current;
+								while (tail.getNode() != null) {
+									tail.getNode().setProcessor(tail.getProcessor() + 1);
+									tail.getNode().setStart(tail.getStartTime());
+									tail = tail.getParent();
+								}
+								exe.shutdown();
+								return;
+							}
+
+							// Find neighbouring nodes
+							Set<Node> neighbours = graph.getNeighbours(current);
+							for (Node n : neighbours) {
+								for (int i = 0; i < numProcessors; i++) {
+									TreeNode candidate = new TreeNode(current, n, i);
+									q.add(candidate);
+								}
+							}
+						}
+					}
 				}
-			}
+
+			});
 		}
-		System.out.println("I FAILED");
-		System.exit(1);
-		return null;
+		try {
+			exe.awaitTermination(1, TimeUnit.HOURS);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return graph;
 	}
 
-    /**
-     * Computes the heuristics for task scheduling which utilises the bottom level.
-     * e.g. longest path to exist task starting with node.
-     */
+	/**
+	 * Computes the heuristics for task scheduling which utilises the bottom level.
+	 * e.g. longest path to exist task starting with node.
+	 */
 	public void computeHeuristics() {
 		for (Node n : graph.getAllNodes()) {
 			setBottomLevel(n);
